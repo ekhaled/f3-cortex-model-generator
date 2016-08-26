9e5258c29f0bad65c6727df01aa50fd183b74123
ekhaled <me.khaled@gmail.com>
2016-08-26 15:59:39 +0100

    working code to generate models

--- src/Ekhaled/Generators/MySQL/Model.php
+++ src/Ekhaled/Generators/MySQL/Model.php
@@ -30,9 +30,10 @@ class Model{
     public function generate()
     {
         $schema = $this->getSchema();
+        $config = $this->config;
 
         foreach($schema as $table){
-            if(!in_array($table['name'], $this->config['exclude'])){
+            if(!in_array($table['name'], $config['exclude'])){
                 $className = $this->className($table['name']);
                 $h = fopen($config['output'].$className.'.php' , 'w');
                 if(fwrite($h, $this->generateModel(
@@ -41,9 +42,9 @@ class Model{
                     $config['extends'],
                     $config['relationNamespace']
                 ))){
-                    output("Generated " .$className." model");
+                    $this->output("Generated " .$className." model");
                 }else{
-                    output('Failed to generate '.$className.' model', true);
+                    $this->output('Failed to generate '.$className.' model', true);
                 }
 
                 fclose($h);
@@ -51,6 +52,69 @@ class Model{
             }
         }
 
+    }
+
+    private function generateModel($schema, $namespace = null, $extends = null, $relationNamespace = '',$classname = null)
+    {
+        $modelTemplate = $this->getTemplate();
+
+        $tablename = strtolower($schema['name']);
+
+        $data = [
+            '{{NAMESPACE}}' => '',
+            '{{CLASSNAME}}' => '',
+            '{{EXTENDS}}' => '',
+            '{{TABLENAME}}' => $tablename,
+        ];
+
+        if($namespace){
+            $data['{{NAMESPACE}}'] = 'namespace '.$namespace.';';
+        }
+
+        if($extends){
+            $data['{{EXTENDS}}'] = 'extends '.$extends;
+        }
+
+        if(!$classname){
+            $classname = $this->className($tablename);
+        }
+
+        $data['{{CLASSNAME}}'] = $classname;
+
+        $fieldConf = [];
+        foreach($schema['columns'] as $column){
+            if(!($column['isPrimaryKey'] && $column['autoIncrement'])){
+                $fieldConf[] = $this->field($column, $relationNamespace);
+            }
+        }
+
+        foreach($schema['relations'] as $rel){
+            $fieldConf[] = $this->virtualfield($rel, $relationNamespace);
+        }
+
+        $modelTemplate = str_replace(array_keys($data), array_values($data), $modelTemplate);
+        $modelTemplate = str_replace('{{FIELDCONF}}', implode(",\n", $fieldConf), $modelTemplate);
+
+        return $modelTemplate;
+
+    }
+
+    private function getTemplate()
+    {
+        return <<<PHP
+<?php
+{{NAMESPACE}}
+
+class {{CLASSNAME}} {{EXTENDS}}
+{
+
+    protected \$fieldConf = [
+{{FIELDCONF}}
+    ],
+    \$table = '{{TABLENAME}}';
+
+}
+PHP;
     }
 
     private function getSchema()
@@ -61,6 +125,91 @@ class Model{
 
     private function className($t, $ns = ''){
         return $ns.ucfirst(strtolower($t));
+    }
+
+    private function field(array $field, $relationNamespace = ''){
+        $template = '        \''.$field['name'].'\' => [
+{{VALUES}}
+        ]';
+
+        $values = [];
+
+        if(isset($field['relation']) && count($field['relation']) > 0){
+            $values[] = '\''.$field['relation']['type'].'\' => \''.$this->className($field['relation']['table'], $relationNamespace).'\'';
+        }else{
+
+            $values[] = '\'type\' => \''.$this->extractType($field['type']).'\'';
+
+            if(!empty($field['default'])){
+                $values[] = '\'default\' => \''.$field['default'].'\'';
+            }
+
+            $values[] = '\'nullable\' => ' . ($field['nullable'] ? 'true' : 'false');
+        }
+
+        $template = str_replace('{{VALUES}}', implode(",\n", array_map(function($val){
+            return '            '.$val;
+        }, $values)), $template);
+
+        return $template;
+    }
+
+    private function virtualfield(array $relation, $relationNamespace = ''){
+        if(isset($relation['via'])){
+            return '        \''.$relation['selfColumn'].'\' => [
+                \''.$relation['type'].'\' => [\''.$this->className($relation['table'], $relationNamespace).'\', \''.$relation['column'].'\', \''.$relation['via'].'\']
+            ]';
+        }else{
+            return '        \''.$relation['table'].'\' => [
+            \''.$relation['type'].'\' => [\''.$this->className($relation['table'], $relationNamespace).'\', \''.$relation['column'].'\']
+        ]';
+        }
+
+    }
+
+    private function extractType($dbType){
+
+        $ints = [1,2,4,8];
+        $varchars = [128, 256, 512];
+
+        $size = null;
+        if(strpos($dbType,'(') && preg_match('/\((.*)\)/',$dbType,$matches)){
+            $values = explode(',',$matches[1]);
+            $size = $values[0];
+        }
+
+        if(stripos($dbType,'tinyint')!==false)
+            $type = 'INT1';
+        elseif(stripos($dbType,'int')!==false && stripos($dbType,'unsigned int')===false)
+            $type = 'INT';
+        elseif(stripos($dbType,'longtext')!==false)
+            $type = 'LONGTEXT';
+        elseif(stripos($dbType,'text')!==false)
+            $type = 'TEXT';
+        elseif(stripos($dbType,'bool')!==false)
+            $type = 'BOOLEAN';
+        elseif(stripos($dbType,'datetime')!==false)
+            $type = 'DATETIME';
+        elseif(stripos($dbType,'date')!==false)
+            $type = 'DATE';
+        elseif(stripos($dbType,'timestamp')!==false)
+            $type = 'TIMESTAMP';
+        elseif(preg_match('/(real|floa|doub)/i',$dbType))
+            $type = 'DOUBLE';
+        else
+            $type = 'VARCHAR';
+
+        if($type == 'VARCHAR' && in_array($size, $varchars)){
+            return $type . $size;
+        }elseif($type == 'VARCHAR'){
+            return 'VARCHAR128';
+        }
+
+        if($type == 'INT' && in_array($size, $ints)){
+            return $type . $size;
+        }
+
+        return $type;
     }
 
     private function output($msg, $err = false){
